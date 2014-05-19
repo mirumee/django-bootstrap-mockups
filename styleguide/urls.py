4941d606ff5e86aa4f6750d5e125ad1294d5c56d
Michał Ociepka <michal@ociepka.info>
2014-05-19 13:23:36 +0200

    Add urls

---
+++ styleguide/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from .views import get_template
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', get_template, {'name': 'index'}, name='index'),
+    url(r'^(?P<name>[a-z0-9-]+)\.html$', get_template, {'is_mockup': True}, name='mockup'),
+    url(r'^(?P<name>[a-z0-9-]+)$', get_template, name='template'),
+)
