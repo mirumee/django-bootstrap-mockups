c71301dd3f48c9b18abce37cda28e8ca2d61d45e
Michał Ociepka <michal@ociepka.info>
2014-05-20 10:22:03 +0200

    Add develop custom command

--- setup.py
+++ setup.py
@@ -1,5 +1,34 @@
 #! /usr/bin/env python
+from subprocess import check_call
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+
+
+class PrepareStaticAndTemplatesCommand(develop):
+
+    @staticmethod
+    def initialize_git_submodules():
+        check_call(['git', 'submodule', 'init'])
+        check_call(['git', 'submodule', 'update'])
+
+    @staticmethod
+    def build_bootstrap_docs_templates():
+        check_call(['(cd jekyll/; jekyll build)'], shell=True)
+
+    @staticmethod
+    def copy_bootstrap_docs_static_files():
+        # todo: windows compatibility
+        static_path = 'styleguide/static/bootstrap_docs'
+        check_call(['mkdir', '-p', static_path])
+        check_call(['cp', '-r', 'bootstrap/docs/dist', static_path])
+        check_call(['cp', '-r', 'bootstrap/docs/assets', static_path])
+
+    def run(self):
+        self.initialize_git_submodules()
+        self.build_bootstrap_docs_templates()
+        self.copy_bootstrap_docs_statics()
+        develop.run(self)
+
 
 CLASSIFIERS = [
     'Environment :: Web Environment',
@@ -31,4 +60,7 @@ setup(
         'django-bootstrap3>=4.4.1'
     ],
     classifiers=CLASSIFIERS,
+    cmdclass={
+        'develop': PrepareStaticAndTemplatesCommand
+    }
 )
