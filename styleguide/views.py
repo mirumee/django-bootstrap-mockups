0caa16b105b2af9a5e28b8fe595068fa47f83fd8
Michał Ociepka <michal@ociepka.info>
2014-05-26 18:39:26 +0200

    Add messages from request

--- styleguide/views.py
+++ styleguide/views.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+from django.contrib import messages
 from django.http import Http404
 from django.template.base import TemplateDoesNotExist
 from django.template.loader import find_template
@@ -7,11 +10,20 @@ from .forms import FormFactory
 from . import get_context
 
 
+def add_messages_from_request(request):
+    data = request.POST or request.GET
+    for name, message in data.items():
+        if name.endswith('_message'):
+            message_type = getattr(messages, name[:-8].upper(), messages.INFO)
+            messages.add_message(request, message_type, message)
+
+
 def get_template(request, name, is_mockup=False):
     context = get_context()
     context['template_name'] = name
     if is_mockup:
-        context['form'] = FormFactory(data=request.POST or request.GET or None)
+        add_messages_from_request(request)
+        context['form'] = FormFactory(data=request.POST or request.GET)
         template = 'styleguide/mockups/%s.html' % (name,)
     else:
         template = 'bootstrap_docs/%s.html' % (name,)
