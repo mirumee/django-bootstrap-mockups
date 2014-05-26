cb35a82c2e2a5273a405199cf30cec54a567b68e
Michał Ociepka <michal@ociepka.info>
2014-05-26 11:04:59 +0200

    Use form factory in mockups

--- styleguide/views.py
+++ styleguide/views.py
@@ -3,7 +3,7 @@ from django.template.base import TemplateDoesNotExist
 from django.template.loader import find_template
 from django.template.response import TemplateResponse
 
-from .forms import BasicExampleForm, ExampleForm
+from .forms import FormFactory
 from . import get_context
 
 
@@ -11,13 +11,10 @@ def get_template(request, name, is_mockup=False):
     context = get_context()
     context['template_name'] = name
     if is_mockup:
+        context['form_factory'] = FormFactory(request)
         template = 'styleguide/mockups/%s.html' % (name,)
     else:
         template = 'bootstrap_docs/%s.html' % (name,)
-        context['forms'] = {
-            'basic': BasicExampleForm(),
-            'small': ExampleForm()
-        }
     try:
         find_template(template)
     except TemplateDoesNotExist:
