3d20fa6c707c16ed9697e9bcd2d1bf72c5d22271
Michał Ociepka <michal@ociepka.info>
2014-05-27 13:02:39 +0200

    Copy fonts to static, PEP8

--- styleguide/__init__.py
+++ styleguide/__init__.py
@@ -3,7 +3,6 @@ import importlib
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-
 STYLE_GUIDE_CONFIG = {
     'title': 'Bootstrap',
     'type': 'less',
